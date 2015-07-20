9510e5e598b408a25af9b81d9fc7d4b626e746c6
David Garber & Jennifer Bolanos <student@epicodus.com>
2015-07-20 14:03:25 -0700

    Describe the doesn't save the message if twilio gives an error.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe Message do
   it "doesn't save the message if twilio gives an error" do
     message = Message.new(:body => 'hi', :to => '11111', :from => '+13106517687')
-    message.save.should be_false
+    message.save.should be false
   end
 end
